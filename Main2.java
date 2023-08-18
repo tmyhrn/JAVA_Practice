ea48e1694a8cca41ff658116a78152f6548a88a0
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 06:20:20 +0000

    Java コード2-4終了

--- Main2.java
+++ Main2.java
@@ -1,5 +1,10 @@
 public class Main2 {
   public static void main(String[] args){
-    System.out.println("私の好きな記号は二重引用符(\")です");
+    int a = 100;
+    a++;
+    int b = 100;
+    b--;
+    System.out.println(a);
+    System.out.println(b);
   }
 }
