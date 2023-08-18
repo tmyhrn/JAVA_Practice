b05ef253f738a5f2d08e60ad763311f16399b150
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 07:43:08 +0000

    Java コード2-13終了

--- Main2.java
+++ Main2.java
@@ -1,10 +1,7 @@
 public class Main2 {
   public static void main(String[] args){
-    int a = 5;
-    int b = 3;
-    int m = Math.max(a, b);
-    int n = Math.min(a, b);
-    System.out.println("比較実験：" + a + "と" + b + "とで大きい方は…" + m);
-    System.out.println("比較実験；" + a + "と" + b + "とで小さい方は…" + n);
+    String age = "31";
+    int n = Integer.parseInt(age);
+    System.out.println("あなたは来年、" + (n+1) + "歳になりますね。");
   }
 }
