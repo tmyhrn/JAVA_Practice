5a1075b3fae926cc8bff7ebfae86d0079587f1b1
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-19 08:34:04 +0900

    コード3-4-2終了

--- Main3.java
+++ Main3.java
@@ -2,6 +2,7 @@ public class Main3 {
   public static void main(String[] args) {
     System.out.println("あなたの運勢を占います");
     int fortune = new java.util.Random().nextInt(4) + 1;
+    /* if構文の場合
     if (fortune == 1) {
       System.out.println("大吉");
     } else if (fortune == 2) {
@@ -9,6 +10,22 @@ public class Main3 {
     } else if (fortune == 3) {
       System.out.println("吉");
     } else {
+      System.out.println("凶");
+    }
+    */
+    
+    //switch構文の場合
+    switch (fortune) {
+      case 1 :
+        System.out.println("大吉");
+        break;
+      case 2 :
+        System.out.println("中吉");
+        break;
+      case 3 :
+        System.out.println("吉");
+        break;
+      default :
       System.out.println("凶");
     }
   }
