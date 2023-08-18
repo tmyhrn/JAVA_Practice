cc0766179b886a62462bb0ed3a001b0b4e73755b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 05:46:56 +0000

    Java コード1-3終了

--- Main1.java
+++ Main1.java
@@ -1,7 +1,9 @@
 public class Main1 {
   public static void main(String[] args){
     int age;
+    age = 20;
+    System.out.println(age + "歳です。");
     age = 30;
-    System.out.println(age+"歳");
+    System.out.println("いや、本当の年齢は" + age + "歳です。");
   }
 }
