c40d75d02ceda1548e21e0536f2ed5f7401e10c0
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 07:35:02 +0000

    Java コード2-11終了

--- Main2.java
+++ Main2.java
@@ -1,7 +1,8 @@
 public class Main2 {
   public static void main(String[] args){
     String name = "すがわら";
-    String message = name + "さん、こんにちは！";
-    System.out.println(message);
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
   }
 }
