90725f471d5c6d01c9c243881bba3627668f85ae
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 05:28:59 +0000

    Java 1.3終了

--- Main1.java
+++ Main1.java
@@ -1,8 +1,7 @@
 public class Main1 {
   public static void main(String[] args){
-    System.out.println("RPG：スッキリ魔王討伐");
-    System.out.println("Ver.0.1 by 湊");
-    System.out.println("<ただいま鋭意学習・製作中>");
-    System.out.println("プログラムを終了します");
+    int age;
+    age = 30;
+    System.out.println(age+"歳");
   }
 }
