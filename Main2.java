a2e4da4f84ed3b42ea4dcd2963a99b64f28fa605
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 07:39:34 +0000

    Java コード2-12-2終了

--- Main2.java
+++ Main2.java
@@ -3,6 +3,8 @@ public class Main2 {
     int a = 5;
     int b = 3;
     int m = Math.max(a, b);
+    int n = Math.min(a, b);
     System.out.println("比較実験：" + a + "と" + b + "とで大きい方は…" + m);
+    System.out.println("比較実験；" + a + "と" + b + "とで小さい方は…" + n);
   }
 }
