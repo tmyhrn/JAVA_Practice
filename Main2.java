047dd23f692af836e522d4035b1e7749496e2ee5
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 07:33:25 +0000

    Java コード2-10終了

--- Main2.java
+++ Main2.java
@@ -1,6 +1,7 @@
 public class Main2 {
   public static void main(String[] args){
-    String msg = "私の年齢は" + 23;
-    System.out.println(msg);
+    String name = "すがわら";
+    String message = name + "さん、こんにちは！";
+    System.out.println(message);
   }
 }
