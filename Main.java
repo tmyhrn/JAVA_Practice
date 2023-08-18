e79681e7c345ee832e30bc86c82014cc17346aaf
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 05:08:45 +0000

    Java 0.2.6終了

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ public class Main {
     System.out.println("5"+"3");
     System.out.println("31 + 31の計算をします");
     System.out.println(31 + 31);
+    int x;
+    x = 6;
+    System.out.println(x * x * 3.14);
   }
 }
