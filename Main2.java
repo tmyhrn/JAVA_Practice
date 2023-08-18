060420a10c20055fddd8de1375073f825e7fe123
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 07:52:19 +0000

    Java コード2-15終了

--- Main2.java
+++ Main2.java
@@ -1,6 +1,9 @@
 public class Main2 {
   public static void main(String[] args){
-    int r = new java.util.Random().nextInt(90);
-    System.out.println("出た数は" + r + "です。");
+    System.out.println("あなたの名前を入力してください。");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    int age = new java.util.Scanner(System.in).nextInt();
+    System.out.println("ようこそ！" + age + "歳の" + name + "さん！");
   }
 }
