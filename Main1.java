d84604dd2dad54c096a370eea0a824b2941cc7a2
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-18 05:53:33 +0000

    Java コード1-5終了

--- Main1.java
+++ Main1.java
@@ -1,12 +1,12 @@
 public class Main1 {
   public static void main(String[] args){
-    double pi = 3.14;
+    final double PI = 3.14;
     int pie = 5;
     System.out.println("半径" + pie + "cmのパイの面積は…");
-    System.out.println(pie * pie* pi);
+    System.out.println(pie * pie* PI);
     System.out.println("パイの半径を倍にします。");
-    pi = 10;
+    PI = 10;
     System.out.println("半径" + pie + "cmのパイの面積は…");
-    System.out.println(pie * pie * pi);
+    System.out.println(pie * pie * PI);
   }
 }
