00480df254bded7287b2c66a40e2e60d4a20a0ca
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-19 08:13:47 +0900

    コード3-2終了

--- Main3.java
+++ Main3.java
@@ -1,11 +1,10 @@
 public class Main3 {
   public static void main(String[] args) {
-    boolean tenki = true;
-    if (tenki == true) {
-    System.out.println("洗濯します。");
-    System.out.println("散歩に行きます。");
-    } else {
-      System.out.println("DVDを見ます。");
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
     }
   }
 }
+//無限ループに陥る
