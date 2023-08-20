4d7579ac4cc9f7df0f2f7b7c3a9710b34fb66d4c
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 11:33:23 +0900

    コード4-2~4終了

--- Main4.java
+++ Main4.java
@@ -1,15 +1,11 @@
 public class Main4 {
   public static void main(String[] args) {
-    int sansu = 20;
-    int kokugo = 30;
-    int rika = 40;
-    int eigo = 50;
-    int syakai = 80;
-    
-    int sum = sansu + kokugo + rika + eigo + syakai;
-    
-    int avg = sum / 5;
-    System.out.println("合計点：" + sum);
-    System.out.println("平均点：" + avg);
+    /*配列の作成手順(基礎)
+    int[] scores;
+    scores = new int [5];
+    */
+    int [] scores = new int [5];
+    int num = scores.length;
+    System.out.println("要素の数：" + num);
   }
 }
