62b66df7a1e8b903a632fbdfbcebdd9d4c039eab
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 11:38:51 +0900

    コード4-6~7終了

--- Main4.java
+++ Main4.java
@@ -1,11 +1,6 @@
 public class Main4 {
   public static void main(String[] args) {
-    /*配列の作成手順(基礎)
-    int[] scores;
-    scores = new int [5];
-    */
     int [] scores = new int [5];
-    scores[1] = 30;
-    System.out.println(scores[1]);
+    System.out.println(scores[0]);
   }
 }
