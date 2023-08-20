35b07d28394f6ba4518679d7f080f4f507bf8ab8
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 13:43:37 +0900

    練習問題4-2終了

--- Main4.java
+++ Main4.java
@@ -1,8 +1,11 @@
 public class Main4 {
   public static void main(String[] args) {
-    int [] points = new int [4];
-    double [] weights = new double [5];
-    boolean [] answers = new boolean [3];
-    String [] names = new String [3];
+    int [] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+    for (int m : moneyList) {
+      System.out.println(m);
+    }
   }
 }
