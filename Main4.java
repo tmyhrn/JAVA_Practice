294b1209700786d5fbe465d4e042c4fe62d6a4fc
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 12:23:40 +0900

    コード4-13~14終了

--- Main4.java
+++ Main4.java
@@ -1,31 +1,16 @@
 public class Main4 {
   public static void main(String[] args) {
-    int [] seq = new int[10];
-    for (int i = 0; i < seq.length; i++) {
-      seq[i] = new java.util.Random().nextInt(4);
-    }
-    
-    //変更後
-    for (int i = 0; i < seq.length; i++) {
-      char [] base = {'A', 'T', 'G', 'C'};
-      System.out.println(base[seq[i]] + "");
-    }
-    /* 変更前
-      switch(seq[i]) {
-        case 0:
-          System.out.println("A");
-          break;
-        case 1:
-          System.out.println("T");
-          break;
-        case 2:
-          System.out.println("G");
-          break;
-        case 3:
-          System.out.println("C");
-          break;
-      }
+    /*基本のfor文
+    int [] scores = {20, 30, 40, 50, 80};
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
     }
     */
+    
+    //拡張for文
+    int [] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
+    }
   }
 }
