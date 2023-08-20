836e71b6b10a9616c1c61d4f8cf3d1ea3b1af0a0
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 11:34:56 +0900

    コード4-5終了

--- Main4.java
+++ Main4.java
@@ -5,7 +5,7 @@ public class Main4 {
     scores = new int [5];
     */
     int [] scores = new int [5];
-    int num = scores.length;
-    System.out.println("要素の数：" + num);
+    scores[1] = 30;
+    System.out.println(scores[1]);
   }
 }
