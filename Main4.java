faa57905f71f6f2704db270120950a0419ed4e7a
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 12:33:43 +0900

    コード4-16終了

--- Main4.java
+++ Main4.java
@@ -1,9 +1,9 @@
+//ガベージコレクション(6行目のデータ)
 public class Main4 {
   public static void main(String[] args) {
-    int [] arrayA = {1, 2, 3};
-    int [] arrayB ;
-    arrayB = arrayA;
-    arrayB[0] = 100;
-    System.out.println(arrayA[0]);
+    boolean judge = true;
+    if (judge == true) {
+      int [] array = {1,2,3,};
+    }
   }
 }
