60eb9c9a5f36f39158021007bc9707018eee1ccb
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 13:40:29 +0900

    練習問題4-1終了

--- Main4.java
+++ Main4.java
@@ -1,7 +1,8 @@
 public class Main4 {
   public static void main(String[] args) {
-    int[][] scores = {{40, 50, 60}, {80, 60, 70}};
-    System.out.println(scores.length);
-    System.out.println(scores[0].length);
+    int [] points = new int [4];
+    double [] weights = new double [5];
+    boolean [] answers = new boolean [3];
+    String [] names = new String [3];
   }
 }
