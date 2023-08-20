d9dbbf2925acc9bf528b053cff11a66fc34af21b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 13:46:57 +0900

    練習問題4-3終了

--- Main4.java
+++ Main4.java
@@ -2,8 +2,8 @@ public class Main4 {
   public static void main(String[] args) {
     int [] counts = null;
     float [] heights = {171.3F, 175.0F};
-
+    System.out.println(counts[1]);
     System.out.println(heights[2]);
   }
 }
-//エラー：NullPointerException・ArrayIndexOutOfBoundsException　
+//エラー：NullPointerException・ArrayIndexOutOfBoundsException
