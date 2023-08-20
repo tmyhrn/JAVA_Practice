df792bfcf5d72ad0784fcd663f14ac9170ba82a2
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 11:21:47 +0900

    練習3-6終了

--- Main3.java
+++ Main3.java
@@ -1,20 +1,17 @@
 public class Main3 {
   public static void main(String[] args) {
-    System.out.print("【メニュー】1:検索 2:登録 3:削除 4:変更＞");
-    int selected = new java.util.Scanner(System.in).nextInt();
-    switch (selected) {
-      case 1 :
-        System.out.println("検索します");
+    System.out.println("【数あてゲーム】");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0〜9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (ans == num) {
+        System.out.println("アタリ！");
         break;
-      case 2 :
-        System.out.println("登録します");
-        break;
-      case 3 :
-        System.out.println("削除します");
-        break;
-      case 4 :
-        System.out.println("変更します");
-        break
+      } else {
+        System.out.println("違います");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
 }
