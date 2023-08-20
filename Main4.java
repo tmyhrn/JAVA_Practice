3cf3c4ac35852de6e27918765122171893f6edaa
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 12:10:07 +0900

    コード4-12-2終了

--- Main4.java
+++ Main4.java
@@ -1,12 +1,8 @@
 public class Main4 {
   public static void main(String[] args) {
     int [] seq = new int[10];
-    
     for (int i = 0; i < seq.length; i++) {
       seq[i] = new java.util.Random().nextInt(4);
-    }
-    
-    for (int i = 0; i < seq.length; i++) {
       switch(seq[i]) {
         case 0:
           System.out.println("A");
