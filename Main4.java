0292ced022931f7ecac951bf2e28698d5472aceb
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 12:38:05 +0900

    コード4-17終了

--- Main4.java
+++ Main4.java
@@ -1,9 +1,8 @@
-//ガベージコレクション(6行目のデータ)
+// nullを代入しているため
 public class Main4 {
   public static void main(String[] args) {
-    boolean judge = true;
-    if (judge == true) {
-      int [] array = {1,2,3,};
-    }
+    int [] array = {1,2,3};
+    array = null;
+    array[0] = 10; 
   }
 }
