2b5f842a13d8482d529172ec18a1cad41d95bda8
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 12:26:33 +0900

    コード4-15終了

--- Main4.java
+++ Main4.java
@@ -1,16 +1,9 @@
 public class Main4 {
   public static void main(String[] args) {
-    /*基本のfor文
-    int [] scores = {20, 30, 40, 50, 80};
-    for (int i = 0; i < scores.length; i++) {
-      System.out.println(scores[i]);
-    }
-    */
-    
-    //拡張for文
-    int [] scores = {20, 30, 40, 50, 80};
-    for (int value : scores) {
-      System.out.println(value);
-    }
+    int [] arrayA = {1, 2, 3};
+    int [] arrayB ;
+    arrayB = arrayA;
+    arrayB[0] = 100;
+    System.out.println(arrayA[0]);
   }
 }
