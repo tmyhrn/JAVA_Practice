be0e72d998d5bd296133e95cf0d0eac9bfacda4b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 13:57:08 +0900

    練習問題4-4終了

--- Main4.java
+++ Main4.java
@@ -1,9 +1,12 @@
 public class Main4 {
   public static void main(String[] args) {
-    int [] counts = null;
-    float [] heights = {171.3F, 175.0F};
-    System.out.println(counts[1]);
-    System.out.println(heights[2]);
+    int [] numbers = {3,4,9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int n : numbers) {
+      if (n == input) {
+        System.out.println("アタリ！");
+      }
+    }
   }
 }
-//エラー：NullPointerException・ArrayIndexOutOfBoundsException
