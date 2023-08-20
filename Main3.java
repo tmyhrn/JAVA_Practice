515e6b0a799fb21276dc012019f8a25acfccc2ea
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-20 11:00:42 +0900

    練習3-4終了

--- Main3.java
+++ Main3.java
@@ -1,13 +1,12 @@
 public class Main3 {
   public static void main(String[] args) {
-    int isHungry = 0;
-    String food = "寿司";
-    System.out.println("こんにちは");
-    if (isHungry == 0) {
-      System.out.println("お腹がいっぱいです");
+    boolean tenki = false;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
     } else {
-      System.out.println(food + "をいただきます");
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
     }
-    System.out.println("ごちそうさまでした");
   }
 }
