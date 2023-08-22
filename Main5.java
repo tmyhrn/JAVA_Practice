34c5e22018a5997c0f1d7900a410ece8e038234f
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-22 23:09:29 +0900

    コード5-3終了

--- Main5.java
+++ Main5.java
@@ -1,10 +1,12 @@
 public class Main5 {
   public static void main(String[] args) {
-    System.out.println("メソッドを呼び出します");
-    hello();
-    System.out.println("メソッドの呼び出しを終了します");
+    methodA();
   }
-  public static void hello() {
-    System.out.println("湊さんここんにちは！");
+  public static void methodA() {
+    System.out.println("おはようございます");
+    methodB();
+  }
+  public static void methodB() {
+    System.out.println("こんにちは");
   }
 }
