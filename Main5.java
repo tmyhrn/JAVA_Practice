7d270ffcbce6f879af7a0159591a741f2c24f32c
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-24 18:50:06 +0900

    コード5-5終了

--- Main5.java
+++ Main5.java
@@ -1,10 +1,11 @@
+//エラー発生(xとyのスコープが)
 public class Main5 {
   public static void main(String[] args) {
-    add(100, 20);
-    add(200, 50);
+    int x = 100;
+    int y = 10;
+    add();
   }
-  //複数の値を受け取るaddメソッド
-  public static void add(int x, int y) {
+  public static void add() {
     int ans = x + y;
     System.out.println(x + "+" + y + "=" + ans);
   }
