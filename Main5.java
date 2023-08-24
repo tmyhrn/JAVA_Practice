13a37f5e0bb6018f03923b2cb2d4b2d4107d1643
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-24 18:57:36 +0900

    コード5-8終了

--- Main5.java
+++ Main5.java
@@ -4,7 +4,7 @@ public class Main5 {
     return ans;
   }
   public static void main(String[] args) {
-    int ans = add(100, 10);
-    System.out.println("100 + 10 = " + ans);
+    //add(10, 20)が30に、add(30, 40)が70に化け、合計で100となる
+    System.out.println(add(add(10, 20), add(30, 40)));
   }
 }
