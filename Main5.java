cbc840b070c550fb2f76b339193e4e83789f0f6f
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-25 18:30:55 +0900

    練習問題5-2-2終了

--- Main5.java
+++ Main5.java
@@ -1,11 +1,15 @@
+//この表現も可能
 public class Main5 {
   public static void main(String[] args) {
     String title = "こんにちは！";
     String address = "example@example.com";
     String text = "お久しぶりです！";
-    email(title, address, text);
+    email();
   }
-  public static void email(String title, String address, String text) {
+  public static void email() {
+    String title = "こんにちは！";
+    String address = "example@example.com";
+    String text = "お久しぶりです！";
     System.out.println(address + "さんにメールを送信");
     System.out.println("件名：" + title);
     System.out.println("本文：" + text);
