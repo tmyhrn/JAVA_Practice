38abc5cf0053822cab5a9cf4ddc5ee088ea19a41
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-25 18:15:37 +0900

    コード5-14終了

--- Main5.java
+++ Main5.java
@@ -1,13 +1,14 @@
 public class Main5 {
-  public static void incArray (int[] array) {
-    for (int i = 0; i < array.length; i++) {
-      array[i]++;
+  public static int[] makeArray(int size) {
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] =i;
     }
+    return newArray;
   }
   public static void main(String[] args) {
-    int[] array = {1,2,3};
-    incArray(array);
-    for (int i : array) {
+    int[] array = makeArray(3);
+    for  (int i : array) {
       System.out.println(i);
     }
   }
