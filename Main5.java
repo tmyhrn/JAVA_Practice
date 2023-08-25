5a78c06c0789e5324f88e7af9ab29f95d21f196e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-25 18:05:47 +0900

    コード5-12終了

--- Main5.java
+++ Main5.java
@@ -1,20 +1,11 @@
 public class Main5 {
-  public static int add(int x, int y) {
-    return x + y;
-  }
-  public static int add(int x, int y, int z) {
-    return x + y + z;
-  }
-  public static double add(double x, double y) {
-    return x + y;
-  }
-  public static String add(String x, String y) {
-    return x + y;
+  public static void printArray(int[] array) {
+    for (int element : array) {
+      System.out.println(element);
+    }
   }
   public static void main(String[] args) {
-    System.out.println("10+20=" + add(10, 20));
-    System.out.println("10+20+30=" + add(10, 20, 30));
-    System.out.println(add(3.5, 2.7));
-    System.out.println(add("Hello", "World"));
+    int[] array = {1, 2, 3};
+    printArray(array);
   }
 }
