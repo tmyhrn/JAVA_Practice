9bd3f87477b73ba2bcb34c9545b0d2c7bc9bb420
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-26 15:51:18 +0900

    練習問題4-4終了

--- Practice.java
+++ Practice.java
@@ -1,16 +1,12 @@
 public class Practice {
   public static void main(String[] args) {
-    System.out.println("【数あてゲーム】");
-    int ans = new java.util.Random().nextInt(10);
-    for (int i = 0; i < 5; i++) {
-      System.out.println("0〜9の数字を入力してください");
-      int num = new java.util.Scanner(System.in).nextInt();
-      if (num == ans) {
-        System.out.println("アタリ！");
-      } else {
-        System.out.println("違います！");
+    int [] numbers = {3,4,9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int i : numbers) {
+      if (i == input){
+       System.out.println("アタリ！"); 
       }
     }
-    System.out.println("ゲームを終了します");
   }
- }
+}
