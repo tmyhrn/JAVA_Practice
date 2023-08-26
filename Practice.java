6f846e48abe77f88afa96848a60493ba634f5b25
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-26 14:50:41 +0900

    練習問題2-1終了

--- Practice.java
+++ Practice.java
@@ -1,8 +1,8 @@
 public class Practice {
   public static void main(String[] args) {
-    int a = 3;
-    int b = 5;
-    int c = a * b;
-    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    int x = 5;
+    int y = 10;
+    String ans = "x + yは" + (x + y);
+    System.out.println(ans);
   }
 }
