60c1af7d22b0886c00c87e265239c57e65ec4c02
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-26 15:44:24 +0900

    練習問題3-6終了

--- Practice.java
+++ Practice.java
@@ -1,20 +1,16 @@
 public class Practice {
   public static void main(String[] args) {
-    System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
-    int selected = new java.util.Scanner(System.in).nextInt();
-    switch (selected) {
-      case 1 :
-        System.out.println("検索します");
-        break;
-      case 2 :
-        System.out.println("登録します");
-        break;
-      case 3 :
-        System.out.println("削除します");
-        break;
-      case 4 :
-        System.out.println("変更します");
-      break;
+    System.out.println("【数あてゲーム】");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0〜9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (num == ans) {
+        System.out.println("アタリ！");
+      } else {
+        System.out.println("違います！");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
-}
+ }
