41322dc5b142c920c634e2403c13834af3763aad
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-26 15:23:18 +0900

    練習問題3-3終了

--- Practice.java
+++ Practice.java
@@ -1,15 +1,16 @@
- public class Practice {
-   public static void main(String[] args) {
-     System.out.println("ようこそ占いの館へ");
-     System.out.println("あなたの名前を入力してください");
-     String name = new java.util.Scanner(System.in).nextLine();
-     System.out.println("あなたの年齢を入力してください");
-     String ageString = new java.util.Scanner(System.in).nextLine();
-     int age = Integer.parseInt(ageString);
-     int fortune = new java.util.Random().nextInt(4);
-     fortune++;
-     System.out.println("占いの結果が出ました");
-     System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-     System.out.println("１：大吉 ２：中吉 ３：吉 ４：凶");
-   }
- }
+public class Practice {
+  public static void main(String[] args) {
+    int isHungry = 0;
+    String food = "ハンバーグ";
+    System.out.println("こんにちは");
+    if (isHungry == 0) {
+      System.out.println("お腹がいっぱいです");
+    } else {
+      System.out.println("はらぺこです");
+    }
+    if (isHungry == 1) {
+      System.out.println(food + "をいただきます");
+    }
+    System.out.println("ごちそうさまでした");
+  }
+}
