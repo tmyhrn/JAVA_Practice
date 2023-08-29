79cc4bb9a47ff8c7612f0ac046d914c27203eadb
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-29 17:29:58 +0900

    コード6-1終了

---
+++ Main6.java
@@ -0,0 +1,15 @@
+// Main6クラス内に、main()・tasu()・hiku()の3つのメソッドがある
+public class Main6 {
+  public static void main(String[] args) {
+    int a = 10; int b = 2;
+    int total = tasu(a, b);
+    int delta = hiku(a, b);
+    System.out.println("足すと" + total + "引くと" + delta);
+  }
+  public static int tasu(int a, int b) {
+    return (a + b);
+  }
+  public static int hiku(int a, int b) {
+    return (a - b);
+  }
+}
