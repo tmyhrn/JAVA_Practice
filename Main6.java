a0520d4da7f99dec089a901cdde00294a6a4ecb9
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-29 17:49:43 +0900

    コード6-7終了

--- Main6.java
+++ Main6.java
@@ -3,8 +3,8 @@ package calcapp.main;
 public class Main6 {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = Main6_1.tasu(a, b);
-    int delta = Main6_1.hiku(a, b);
+    int total = calcapp.logics.Main6_1.tasu(a, b);
+    int delta = calcapp.logics.Main6_1.hiku(a, b);
     System.out.println("足すと" + total + "引くと" + delta);
   }
 }
