75bfafbec75d5e74da27aaa2f49c7eec52d6eff1
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-29 17:45:05 +0900

    コード6-5~6終了

--- Main6.java
+++ Main6.java
@@ -1,3 +1,5 @@
+package calcapp.main;
+
 public class Main6 {
   public static void main(String[] args) {
     int a = 10; int b = 2;
--- Main6_1.java
+++ Main6_1.java
@@ -1,3 +1,5 @@
+package calcapp.logics;
+
 public class Main6_1 {
   public static int tasu(int a, int b) {
     return (a + b);
