45683af1536d2c471feb1a5f6c956d17144937f0
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-30 11:04:15 +0900

    練習4

--- Practice.java
+++ Practice.java
@@ -1,11 +1,31 @@
-//1〜10までの整数をカンマ区切りで横並びで表示(10にはカンマをつけない)
-//for文を用いて、初期値1から1ずつ数を増やしていき、10未満の場合はカンマで区切る
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入。[0]~[10]の値の奇数のみカンマ区切り、[11]~[19]の値は偶数をカンマ区切りで表示さ
+//
 public class Practice {
-  public static void main(String[] args) {
-    for (int i = 1; i <= 10; i++) {
-      System.out.print(i);
-      if (i < 10) {
-        System.out.print(",");
+  public static void main (String[] args) {
+    int[] array = new int[20]; //配列の宣言
+    
+    for (int i = 0; i < 20; i++) {
+      array[i] = i * 5; //0から5ずつ加算して代入
+    }
+    
+    //奇数の値をカンマ区切りで表示
+    for (int i = 0; i <= 10; i++) {
+      if (array[i] % 2 != 0) {
+        System.out.print(array[i]);
+        if (i <= 10) {
+          System.out.print(", ");
+        }
+      }
+    }
+    System.out.println(); //改行
+    
+    //偶数の値をカンマ区切りで表示
+    for (int i = 11; i < 20; i++) {
+      if (array[i] % 2 == 0) {
+        System.out.print(array[i]);
+        if (i < 20) {
+          System.out.print(", ");
+        }
       }
     }
   }
