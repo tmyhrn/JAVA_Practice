588509f63014a387eaeb250e9136933a63f6a85b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-30 11:07:59 +0900

    練習1-2

--- Practice.java
+++ Practice.java
@@ -1,31 +1,8 @@
-//要素数20の配列を宣言し、要素に0から5ずつ加算して代入。[0]~[10]の値の奇数のみカンマ区切り、[11]~[19]の値は偶数をカンマ区切りで表示さ
+//初期値を9とし、2ずつ減少させながらfor文を用いて繰り返し処理を行うようにして奇数表現
 public class Practice {
   public static void main (String[] args) {
-    int[] array = new int[20]; //配列の宣言
-    
-    for (int i = 0; i < 20; i++) {
-      array[i] = i * 5; //0から5ずつ加算して代入
-    }
-    
-    //奇数の値をカンマ区切りで表示
-    for (int i = 0; i <= 10; i++) {
-      if (array[i] % 2 != 0) {
-        System.out.print(array[i]);
-        if (i <= 10) {
-          System.out.print(", ");
-        }
-      }
-    }
-    System.out.println(); //改行
-    
-    //偶数の値をカンマ区切りで表示
-    for (int i = 11; i < 20; i++) {
-      if (array[i] % 2 == 0) {
-        System.out.print(array[i]);
-        if (i < 20) {
-          System.out.print(", ");
-        }
-      }
+    for (int i = 9; i >= 1; i-=2) {
+      System.out.println(i);
     }
   }
 }
