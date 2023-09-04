64e7114c967da5ed12c5a2638e8244c762634337
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-04 11:40:09 +0900

    練習1(2回目)

--- Practice.java
+++ Practice.java
@@ -1,7 +1,8 @@
-//初期値を9とし、2ずつ減少させながらfor文を用いて繰り返し処理を行うようにして奇数表現
+//1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+
 public class Practice {
-  public static void main (String[] args) {
-    for (int i = 9; i >= 1; i-=2) {
+  public static void main(String[] args) {
+    for (int i = 10; i >= 2; i -= 2) {
       System.out.println(i);
     }
   }
