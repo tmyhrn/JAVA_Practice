c3309628ebc1449a95ae0ead290ecd868d877bf1
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-07 07:37:15 +0900

    練習4

--- Practice.java
+++ Practice.java
@@ -1,11 +1,31 @@
-//1〜10までの整数をカンマ区切りで横並びで表示（10にはカンマをつけない）させる問題
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。
+//[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる問題
 
 public class Practice {
   public static void main(String[] args) {
-    for (int i = 1; i < 11; i ++) {
-      System.out.print(i);
-      if (i < 10) {
-        System.out.print(",");
+    int[] array = new int[20];
+    
+    for (int i = 0; i < 20; i ++) {
+      array[i] = i * 5;
+    }
+    
+    for (int i = 0; i <= 10; i++) {
+      if (array[i] % 2 != 0) {
+        System.out.print(array[i]);
+        if (i <= 10) {
+          System.out.print(",");
+        }
+      }
+    }
+    
+    for (int i = 11; i < 20; i ++) {
+      if (array[i] % 2 == 0) {
+        if (array[i] % 2 == 0) {
+          System.out.print(array[i]);
+          if (i < 20) {
+            System.out.print(",");
+          }
+        }
       }
     }
   }
