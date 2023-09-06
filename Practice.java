df9a7846aa9b4e287a8ab5b22ea8e2020dda2813
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-06 20:33:01 +0900

    練習2

--- Practice.java
+++ Practice.java
@@ -1,11 +1,16 @@
-//1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+//1〜10までの偶数が何個あるのか、偶数の合計は幾つになるかを表示させる問題
 
 public class Practice {
   public static void main(String[] args) {
-    for (int i = 10; i > 1; i --) {
+    int count = 0;
+    int sum = 0;
+    for (int i = 10; i > 1; i--) {
       if (i % 2 == 0) {
-        System.out.println(i);
+        count++;
+        sum += i;
       }
     }
+    System.out.println("偶数の数：" + count);
+    System.out.println("偶数の合計：" + sum);
   }
 }
