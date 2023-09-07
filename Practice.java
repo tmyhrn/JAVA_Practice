41628721c24d4f98ec04b49c0c06435eb7f7c316
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-07 18:18:30 +0900

    Fibonacci sequence(0~)

--- Practice.java
+++ Practice.java
@@ -1,32 +1,20 @@
-//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。
-//[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる問題
+//フィボナッチ数列(0から始まるパターン)
 
 public class Practice {
   public static void main(String[] args) {
-    int[] array = new int[20];
+    int f0, f1, fn;
     
-    for (int i = 0; i < 20; i ++) {
-      array[i] = i * 5;
-    }
+    f0 = 0;
+    System.out.println(f0);
     
-    for (int i = 0; i <= 10; i++) {
-      if (array[i] % 2 != 0) {
-        System.out.print(array[i]);
-        if (i <= 10) {
-          System.out.print(",");
-        }
-      }
-    }
+    f1 = 1;
+    System.out.println(f1);
     
-    for (int i = 11; i < 20; i ++) {
-      if (array[i] % 2 == 0) {
-        if (array[i] % 2 == 0) {
-          System.out.print(array[i]);
-          if (i < 20) {
-            System.out.print(",");
-          }
-        }
-      }
+    for (int i = 2; i <= 10; i++) {
+      fn = f0 + f1;
+      System.out.println(fn);
+      f0 = f1;
+      f1 = fn;
     }
   }
 }
