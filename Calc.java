ee10fb85d436342f36686a9425a9b590fbc0671f
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-08 18:43:23 +0900

    コード6-8修正

--- Calc.java
+++ Calc.java
@@ -1,9 +1,10 @@
 package calcapp.main; 
+import calcapp.logics.CalcLogic;
 
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = calcapp.logics.CalcLogic.tasu(a, b);
+    int total = CalcLogic.tasu(a, b);
     int delta = calcapp.logics.CalcLogic.hiku(a, b);
     System.out.println("足すと" + total + "、引くと" + delta);
   }
