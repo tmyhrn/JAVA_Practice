b9bbe2e20393982a1b7811d1890087e5ad9498ca
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-08 18:36:18 +0900

    コード6-5

--- Calc.java
+++ Calc.java
@@ -1,9 +1,11 @@
+package calcapp.main; //エラーが発生
+
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 2;
     int total = CalcLogic.tasu(a, b);
     int delta = CalcLogic.hiku(a, b);
-    //CalcLogicファイルに記載されている「tasu」と「hiku」を呼び出すために、「CalcLogic.」と記載
+    //CalcLogicファイルに記載されている「tasu」と「hiku」を呼び出すために、「CalcLogic.」と記載している
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
