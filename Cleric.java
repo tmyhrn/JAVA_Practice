2caf5aca81e9b4dafc640e92b9606c0953731252
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-11 16:46:03 +0900

    練習問題8-4終了

--- Cleric.java
+++ Cleric.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public class Cleric {
-  String name;
+  String name = "聖職者";
   int hp = 50;
   final int MAX_HP = 50;
   int mp = 10;
@@ -10,5 +12,14 @@ public class Cleric {
     this.hp = this.MAX_HP;
     this.mp -= 5;
     System.out.println("HPが最大まで回復した！");
+  }
+  
+  public int pray(int sec) {
+    System.out.println(this.name + "は、"　+ sec + "秒間天に祈った！");
+    int recover = new Random().nextInt(3) + sec;
+    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+    this.mp += recoverActual;
+    System.out.println("MPが" + recoverActual + "回復した！");
+    return recoverActual;
   }
 }
