da6bf381c35ce11ed2a3296ed1a3fc147b871339
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-11 16:39:28 +0900

    練習問題8-3終了

--- Cleric.java
+++ Cleric.java
@@ -4,4 +4,11 @@ public class Cleric {
   final int MAX_HP = 50;
   int mp = 10;
   final int MAX_MP = 10;
+  
+  public void selfAid() {
+    System.out.println(this.name + "は、セルフエイドを唱えた！");
+    this.hp = this.MAX_HP;
+    this.mp -= 5;
+    System.out.println("HPが最大まで回復した！");
+  }
 }
