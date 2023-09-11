6190a6d9e6a4e6bd43985adfd0aa43b5d8bca58d
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-11 17:23:20 +0900

    コード9-3~4終了

--- Hero.java
+++ Hero.java
@@ -1,27 +1,9 @@
 public class Hero {
   String name;
   int hp;
-  
-  public void sleep() {
-    this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！");
-  }
-  
-  public void sit(int sec) {
-    this.hp += sec;
-    System.out.println(this.name + "は、" + sec + "秒座った！");
-    System.out.println("HPが" + sec + "ポイント回復した！");
-  }
-  
-  public void slip() {
-    this.hp -= 5;
-    System.out.println(this.name + "は転んだ！");
-    System.out.println("5のダメージ");
-  }
-  
-  public void run() {
-    System.out.println(this.name + "は、逃げ出した！");
-    System.out.println("終了！");
-    System.out.println("最終HPは" + this.hp + "でした");
+  Sword sword;
+  public void attack() {
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に5ポイントのダメージを与えた");
   }
 }
--- Practice.java
+++ Practice.java
@@ -1,20 +1,5 @@
-//フィボナッチ数列(から始まるパターン)
-
 public class Practice {
   public static void main(String[] args) {
-    int f1, f2, fn;
     
-    f1 = 1;
-    System.out.println(f1);
-    
-    f2 = 1;
-    System.out.println(f2);
-    
-    for (int i = 2; i < 10; i++) {
-      fn = f1 + f2;
-      System.out.println(fn);
-      f1 = f2;
-      f2 = fn;
-    }
   }
 }
