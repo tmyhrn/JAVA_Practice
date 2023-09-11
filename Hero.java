5823cedb43486d5ee92a8d72251a293d148d5645
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-11 16:06:15 +0900

    コード8-7終了

--- Hero.java
+++ Hero.java
@@ -1,9 +1,9 @@
 public class Hero {
   String name;
   int hp;
-  public void attack();
-  public void sleep();
-  public void sit(int sec);
-  public void slip();
-  public void run();
+  
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！");
+  }
 }
