f96a08f1e681d55b21a270ce9b3c4c6ea28c110a
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-14 11:37:20 +0900

    コード10-3終了

--- SuperHero.java
+++ SuperHero.java
@@ -1,19 +1,7 @@
-public class SuperHero {
-  String name = "ミナト";
-  int hp = 100;
+//Heroクラスの継承を行う
+public class SuperHero extends Hero {
   boolean flying;
-  
-  //戦うメソッド
-  public void attack(Matango m) {
-    System.out.println(this.name + "の攻撃！");
-    m.hp -= 5;
-    System.out.println("5ポイントのダメージを与えた！");
-  }
-  
-  //逃げるメソッド
-  public void run() {
-    System.out.println(this.name + "は逃げ出した！");
-  }
+  //戦うメソッド・逃げるメソッドは継承しているので記述する必要がない
   
   //飛ぶメソッド
   public void fly() {
