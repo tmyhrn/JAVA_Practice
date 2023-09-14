06ca95734c2f0764946b7cb97830a9c835fd3229
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-14 11:50:22 +0900

    コード10-7終了

--- Hero.java
+++ Hero.java
@@ -2,13 +2,18 @@ public class Hero {
   String name = "ミナト";
   int hp = 100;
   
+  //転ぶメソッド(finalがついているのでオーバーライド不可)
+  public final void slip() {
+    this.hp -= 5;
+    System.out.println(this.name + "は転んだ！");
+    System.out.println("5のダメージ");
+  }
   //戦うメソッド
   public void attack(Matango m) {
     System.out.println(this.name + "の攻撃！");
     m.hp -= 5;
     System.out.println("5ポイントのダメージを与えた！");
   }
-  
   //逃げるメソッド
   public void run() {
     System.out.println(this.name + "は逃げ出した！");
