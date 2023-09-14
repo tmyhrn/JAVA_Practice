6f1227c2df018df854bfbef5ecd5824a5464fb5e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-14 11:46:28 +0900

    コード10-5~6終了

--- Main10.java
+++ Main10.java
@@ -1,5 +1,7 @@
 public class Main10 {
   public static void main(String[] args) {
+    Hero h = new Hero();
+    h.run();
     SuperHero sh = new SuperHero();
     sh.run();
   }
--- SuperHero.java
+++ SuperHero.java
@@ -8,10 +8,13 @@ public class SuperHero extends Hero {
     this.flying = true;
     System.out.println("飛び上がった！");
   }
-  
   //着地するメソッド
   public void land() {
     this.flying = false;
     System.out.println("着地した！");
+  }
+  //逃げるメソッド(Heroクラスにも記載しているが、ここではオーバーライドする)
+  public void run() {
+    System.out.println(this.name + "は撤退した！");
   }
 }
