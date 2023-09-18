607da04136d7cae47f7df2d8cbe3d460f3069f0b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-18 10:55:34 +0900

    コード8-8終了

--- Hero.java
+++ Hero.java
@@ -6,15 +6,21 @@ public class Hero {
   }
   public void sleep() {
     this.hp = 100;
-    System.out.println(this.name + "は眠って回復した”");
+    System.out.println(this.name + "は眠って回復した！");
   }
   public void sit(int sec) {
-    
+    this.hp += sec;
+    System.out.println(this.name + "は、"+ sec + "秒座った！");
+    System.out.println("HPが" + sec + "ポイント回復した！");
   }
   public void slip() {
-    
+    this.hp -= 5;
+    System.out.println(this.name + "は転んだ！");
+    System.out.println("5のダメージ！");
   }
   public void run() {
-    
+    System.out.println(this.name + "は逃げ出した！");
+    System.out.println("終了！");
+    System.out.println("最終HPは" + this.name + "でした！");
   }
 }
