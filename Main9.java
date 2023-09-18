35632e80b7161f26ac622c6cfc59445a0556317e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-18 12:09:15 +0900

    コード9-10~11終了

--- Main9.java
+++ Main9.java
@@ -1,18 +1,7 @@
 public class Main9 {
   public static void main(String[] args) {
-    Hero h1 = new Hero();
-    h1.name = "ミナト";
-    System.out.println(h1.hp);
-    
-    Hero h2 = new Hero();
-    h2.name = "アサカ";
-    h2.hp = 100;
-    
-    Wizard w = new Wizard();
-    w.name = "スガワラ";
-    w.hp = 50;
-    w.heal(h1);
-    w.heal(h2);
-    w.heal(h2);
+    Hero h = new Hero("ミナト");
+    System.out.println(h.name);
+    System.out.println(h.hp);
   }
 }
