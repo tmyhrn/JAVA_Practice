228285d968041f0a24effdaedcfcb00df3bc2e0b
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-18 10:47:46 +0900

    コード8-2~4終了

--- Hero.java
+++ Hero.java
@@ -1,25 +1,19 @@
 public class Hero {
-  String name = "ミナト";
-  int hp = 100;
-  
-  public Hero() {
-    System.out.println("Heroのコンストラクタが動作");
+  String name;
+  int hp;
+  public void attack() {
+    
   }
-  
-  //転ぶメソッド(finalがついているのでオーバーライド不可)
-  public final void slip() {
-    this.hp -= 5;
-    System.out.println(this.name + "は転んだ！");
-    System.out.println("5のダメージ");
+  public void sleep() {
+    
   }
-  //戦うメソッド
-  public void attack(Matango m) {
-    System.out.println(this.name + "の攻撃！");
-    m.hp -= 5;
-    System.out.println("5ポイントのダメージを与えた！");
+  public void sit(int sec) {
+    
   }
-  //逃げるメソッド
+  public void slip() {
+    
+  }
   public void run() {
-    System.out.println(this.name + "は逃げ出した！");
+    
   }
 }
