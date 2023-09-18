f5f4862ac3638dfb1c3fc5e7bc275ac5df1ed030
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-18 12:00:58 +0900

    コード9-5~6終了

--- Main9.java
+++ Main9.java
@@ -1,12 +1,18 @@
 public class Main9 {
   public static void main(String[] args) {
-    Sword s = new Sword();
-    s.name = "炎の剣";
-    s.damage = 10;
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    h.sword = s;
-    System.out.println("現在の武器は、" + h.sword.name);
+    Hero h1 = new Hero();
+    h1.name = "ミナト";
+    h1.hp = 100;
+    
+    Hero h2 = new Hero();
+    h2.name = "アサカ";
+    h2.hp = 100;
+    
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    w.heal(h1);
+    w.heal(h2);
+    w.heal(h2);
   }
 }
--- Sword.java
+++ Sword.java
@@ -1,4 +1,9 @@
 public class Sword {
   String name;
   int damage;
+  
+  public void heal(Hero h) {
+    h.hp += 10;
+    System.out.println(h.name + "のHPを10回復した！");
+  }
 }
