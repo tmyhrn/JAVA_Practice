ad4dfdc8496c8afc4bb6f52111a4145967de2862
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-18 11:01:28 +0900

    コード8-9~12終了

--- Main8.java
+++ Main8.java
@@ -1,5 +1,8 @@
 public class Main8 {
   public static void main(String[] args) {
-    
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
   }
 }
